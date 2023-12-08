eef68252f8b92b746f419b7d45d9f44a7b86a66b
zoomasochist <110025755+zoomasochist@users.noreply.github.com>
2023-12-08 22:53:38 +1030

    fix camera, model, etc.

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ fn main() {
     let native_options = eframe::NativeOptions {
         centered: true,
         renderer: eframe::Renderer::Glow,
+        depth_buffer: 24,
         viewport: egui::ViewportBuilder::default()
             .with_decorations(false)
             .with_transparent(true),
         ..Default::default()
     };
-    
+
     eframe::run_native("Goonie Buddy",
         native_options,
         Box::new(|cc| Box::new(MyEguiApp::new(cc)))
@@ -35,7 +36,7 @@ impl MyEguiApp {
             rect,
             callback: std::sync::Arc::new(egui_glow::CallbackFn::new(move |info, painter| {
                 with_three_d(painter.gl(), |three_d| {
-                    three_d.frame(FrameInput::new(&three_d.context, &info, painter), 0.);
+                    three_d.frame(FrameInput::new(&three_d.context, &info, painter));
                 })
             })),
         };
@@ -48,7 +49,7 @@ impl eframe::App for MyEguiApp {
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {    
         let resp =
             egui::CentralPanel::default().frame(egui::Frame::none()).show(ctx, |ui| {
-                ui.style_mut().visuals.extreme_bg_color = egui::Color32::TRANSPARENT;
+                // ui.style_mut().visuals.extreme_bg_color = egui::Color32::TRANSPARENT;
                 egui::Frame::canvas(ui.style()).show(ui, |ui| {
                     self.draw_model(ui);
                 });
@@ -89,11 +90,6 @@ impl FrameInput<'_> {
     ) -> Self {
         use three_d::*;
 
-        unsafe {
-            use glow::HasContext as _;
-            context.disable(glow::FRAMEBUFFER_SRGB);
-        }
-
         // Constructs a screen render target to render the final image to
         let screen = painter.intermediate_fbo().map_or_else(
             || {
@@ -130,6 +126,7 @@ impl FrameInput<'_> {
             width: clip_rect.width_px as _,
             height: clip_rect.height_px as _,
         };
+        
         Self {
             screen,
             scissor_box,
@@ -148,26 +145,37 @@ pub struct ThreeDApp {
 impl ThreeDApp {
     pub fn new(gl: std::sync::Arc<glow::Context>) -> Self {
         let context = Context::from_gl_context(gl).unwrap();
-        // Create a camera
-        let camera = Camera::new_perspective(
-            Viewport::new_at_origo(0, 0),
-            vec3(0., 0., 0.),
-            vec3(0., 0., 0.),
-            vec3(0., 0., 0.),
-            degrees(45.),
-            0.1,
-            1000.,
-        );
 
         let mut loaded = three_d_asset::io::load(&[GLB_PATH]).unwrap();
         let mut cpu_model: CpuModel = loaded.deserialize("glb").unwrap();
-
-        cpu_model
-            .geometries
-            .iter_mut()
-            .for_each(|part| part.compute_normals());
         
         let mut model = Model::<PhysicalMaterial>::new(&context, &cpu_model).unwrap();
+
+        model.iter_mut().for_each(|m| m.material.render_states.cull = Cull::Back);
+
+        let mut bounding_box = AxisAlignedBoundingBox::EMPTY;
+        model.iter().for_each(|part| bounding_box.expand_with_aabb(&part.aabb()));
+        println!("Model bounding box: {:?} centered at {:?}", bounding_box, bounding_box.center());
+
+        // let camera = Camera::new_perspective(
+        //     /* Viewport */ Viewport::new_at_origo(0, 0),
+        //     /* Position */ bounding_box.center(),
+        //     /*  Target  */ bounding_box.center(),
+        //     /*    Up    */ vec3(0., 1., 0.),
+        //     /*  Height  */ degrees(90.),
+        //     /*  Z near  */ 0.01,
+        //     /*  Z far   */ 10000.,
+        // );
+
+        let camera = Camera::new_orthographic(
+            /* Viewport */ Viewport::new_at_origo(0, 0),
+            /* Position */ bounding_box.center(),
+            /*  Target  */ bounding_box.center(),
+            /*    Up    */ vec3(0., 1., 0.),
+            /*  Height  */ 10.,
+            /*  Z near  */ 0.01,
+            /*  Z far   */ 10000.,
+        );
 
         Self {
             context,
@@ -176,20 +184,56 @@ impl ThreeDApp {
         }
     }
 
-    pub fn frame(&mut self, frame_input: FrameInput<'_>, angle: f32) -> Option<glow::Framebuffer> {
+    fn fit_camera(&mut self, bounding_box: &AxisAlignedBoundingBox) {
+        let centre = bounding_box.center();
+        let size = bounding_box.size();
+        let max_size = size.x.max(size.y).max(size.z);
+
+        let up = *self.camera.up();
+        let target = *self.camera.target();
+        self.camera.set_view(
+            vec3(max_size, max_size, max_size) + vec3(10., 10., 10.),
+            target,
+            up,
+        );
+
+        self.camera.set_orthographic_projection(
+            100.,
+            -max_size * 4.,
+            max_size * 4.,
+        );
+    }
+
+    // // https://discourse.threejs.org/t/camera-zoom-to-fit-object/936/2
+    // fn fit_camera(&mut self, bounding_box: &AxisAlignedBoundingBox) {
+    //     let mut new_pos = *self.camera.position();
+    //     let fov: f32 = 90. * (std::f32::consts::PI / 180.);
+
+    //     let centre = bounding_box.center();
+    //     let size   = bounding_box.size();
+    //     let max_dim = size.x.max(size.y).max(size.z);
+
+    //     let mut camera_z = max_dim / 4. * (fov * 2.).tan();
+    //     // Zoom out a little
+    //     camera_z *= 1.25;
+    //     new_pos.z = centre.z + camera_z;
+        
+    //     let up = *self.camera.up();
+    //     self.camera.set_view(new_pos, centre, up);
+    // }
+
+    pub fn frame(&mut self, frame_input: FrameInput<'_>) -> Option<glow::Framebuffer> {
+        let mut bounding_box = AxisAlignedBoundingBox::EMPTY;
+        self.model.iter().for_each(|part| bounding_box.expand_with_aabb(&part.aabb()));
+
         // Ensure the viewport matches the current window viewport which changes if the window is resized
         self.camera.set_viewport(frame_input.viewport);
-
-        // Set the current transformation of the triangle
-        // self.model
-        //     .set_transformation(Mat4::from_angle_y(radians(angle)));
-
-        let light0 = AmbientLight::new(&self.context, 1., Srgba::WHITE);
-        // There has to be at least one directional light in the scene or three-d crashes.
-        // Some error about normal maps.
-        let light1 = DirectionalLight::new(&self.context, 1.0, Srgba::WHITE, &vec3(0.0, 0.5, 0.5));    
+        self.fit_camera(&bounding_box);
 
 
+        let light0 = DirectionalLight::new(&self.context, 1.0, Srgba::WHITE, &vec3(0.0, -0.5, -0.5));
+        let light1 = DirectionalLight::new(&self.context, 1.0, Srgba::WHITE, &vec3(0.0, 0.5, 0.5));
+    
         // Get the screen render target to be able to render something on the screen
         frame_input
             .screen
@@ -198,6 +242,6 @@ impl ThreeDApp {
             // Render the triangle with the color material which uses the per vertex colors defined at construction
             .render_partially(frame_input.scissor_box, &self.camera, &self.model, &[&light0, &light1]);
 
-        frame_input.screen.into_framebuffer() // Take back the screen fbo, we will continue to use it.
+        frame_input.screen.into_framebuffer()
     }
 }
